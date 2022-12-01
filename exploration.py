4414283d1c45e92e65c409be543c435c5caf7388
mikerabs <mrabayda4@icloud.com>
2022-12-01 14:58:45 -0500

    second commit, created pairplot svg and correlation heatmap from exploration.py

--- exploration.py
+++ exploration.py
@@ -3,7 +3,7 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-df = pd.read_csv("fangraphs-minor-league-leaders-advanced.csv")
+df = pd.read_csv("forPairPlot.csv")
 print(df.head())
 
 plt.subplots(figsize = (25,8))
@@ -16,9 +16,34 @@ plt.savefig("heatmap.svg")
 #g = sns.FacetGrid(df,col = "Level")
 #wOBAplot = g.map(sns.lmplot, "BABIP", "wOBA")
 
-wOBAplot = sns.lmplot(x = "wRC", y = "wOBA", hue = "Level",col = "Level", col_wrap =3,robust = False, data = df, order = 1, ci = 95)
+#wOBAplot2 = sns.lmplot(x = "wRC", y = "wOBA", hue = "Level",col = "Level", col_wrap =3,robust = False, data = df, order = 1, ci = 95)
 
-plt.savefig("wOBAplot.svg")
+# Visualizing multicollinearity between independent features using a heatmap  
+  
+corr = df.corr()  
+print('Pearson correlation coefficient matrix for each independent variable: \n', corr)  
+  
+# Masking the diagonal cells   
+masking = np.zeros_like(corr, dtype = np.bool)  
+np.fill_diagonal(masking, val = True)  
+  
+# Initializing a matplotlib figure  
+figure, axis = plt.subplots(figsize = (12, 9))  
+  
+# Generating a custom colormap  
+c_map = sns.diverging_palette(223, 14, as_cmap = True, sep = 100)  
+c_map.set_bad('grey')  
+  
+# Displaying the heatmap with the masking and the correct aspect ratio  
+sns.heatmap(corr, mask = masking, cmap = c_map, vmin = -1, vmax = 1, center = 1, linewidths = 1)  
+figure.suptitle('Heatmap visualizing Pearson Correlation Coefficient Matrix', fontsize = 14)  
+axis.tick_params(axis = 'both', which = 'major', labelsize = 10)
+
+plt.savefig("heatmapcorr.svg")
+
+#wOBAplot = sns.pairplot(data = df, hue = "Level",height = 2)
+
+#plt.savefig("wOBAplot2.svg")
 
 #OBP vs O-Swing% order 2
 
